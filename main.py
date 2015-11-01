ef91431dfed5aa6494a0665695f44794e9fd6734
Ashkan <ashkan.pk@gmail.com>
2015-10-31 20:51:38 -0400

    Now adds gifs for each article item

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from meteorInterface import meteorInterface
 from RedditFetcher import RedditFetcher
+from GIFFY import GIFFY
 
-'''interface_obj = meteorInterface()
-
-title = "16 best euphemisms for sex that you don't know"
-content = [
-  {'body': """Coffee.
-Me and a girl I dated always agreed that sex in the morning was better than coffee so thats what we started calling it. And occasionally when we were leaving outings with friends one of us would ask "Do we need to stop and get coffee filters?" And everyone just gave us funny looks.
-""", 'gif_url': 'https://media.giphy.com/media/HEgG7AcV99tyE/giphy.gif'}
-  ]
-
-print interface_obj.insertByContent(title, content)
-'''
 interface_obj = meteorInterface()
 
 myobj = RedditFetcher()
-submissions = myobj.getThreads(1)
+submissions = myobj.getThreads(30)
 
 for sub in submissions:
-    content = []
-    comments = myobj.gatherAllEntries(10, sub.url)
-    for key in comments:
-        #print str(comments[key].body)
-        content.append({'body': comments[key].body, 'gif_url':'', 'keywords': myobj.getKeywords(comments[key].body), 'score': comments[key].score})
-    interface_obj.insertByContent(sub.title, content)
- 
+  content = []
+  comments = myobj.gatherAllEntries(10, sub.url)
+  for key in comments:
+    #print str(comments[key].body)
+    body = comments[key].body
+    keywords = myobj.getKeywords(comments[key].body)
+    
+    gif_obj = GIFFY()
+    gif_url = gif_obj.findGIF(keywords)
+    content.append({'body': body,
+                    'gif_url': gif_url,
+                    'keywords': keywords,
+                    'score': comments[key].score
+                  })
+
+  interface_obj.insertByContent(sub.title, content)
+
