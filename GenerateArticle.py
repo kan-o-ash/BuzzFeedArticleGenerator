43aefdb7a762d8a75b3c1852698fb76f4d3a75e8
Ashkan <ashkan.pk@gmail.com>
2015-11-03 15:15:15 -0500

    Finished basic article generation.

--- GenerateArticle.py
+++ GenerateArticle.py
@@ -1,32 +1,33 @@
+from GifFinder import GifFinder
+
 class GenerateArticle(object):
 
-	def processTitle(self, article):
-		return
+    def processTitle(self, article):
+        return
 
-	def goodComment(self, item):
-		return True
+    def goodComment(self, item):
+        return True
 
-	def processComments(self, article):
-		final_items = []
+    def processComments(self, article):
+        final_items = []
 
-		for item in article.items:
-			if self.goodComment(item):
-				final_items.append(item)
+        for item in article.content:
+            if self.goodComment(item):
+                final_items.append(item)
 
-		article.items = final_items
-		
+        article.content = final_items
+        
 
-	def addGifs(self, article):
-		for idx, item in enumerate(Article.items):
-			GIFFY.addGIF(item)
-			Article.items[idx] = item
+    def addGifs(self, article):
+        gif = GifFinder()
+        for idx, item in enumerate(article.content):
+            item['gif_url'] = gif.getGIF(item['text'])
 
+    def generate(self, article):
+        self.processComments(article)
 
-	def generate(self, article):
-		self.processComments(article)
+        self.addGifs(article)
 
-		# addGifs(article)
+        self.processTitle(article)
 
-		self.processTitle(article)
-
-		return
+        return
--- controller.py
+++ controller.py
@@ -1,21 +1,26 @@
 from FetchThread import FetchThread
 from GenerateArticle import GenerateArticle
-from CommitArticle import CommitArticle
+from ArticleCommit import ArticleCommit
 from Article import Article
 
+import time
 
 class Controller:
-	def run(self):
-		url = "https://www.reddit.com/r/AskReddit/comments/3qynmv/you_gain_the_ability_to_put_a_30s_waiting_period/"
+    def createArticle(self, url):
+        fetcher = FetchThread()
+        article = fetcher.getArticle(url)
 
-		fetcher = FetchThread()
-		article = fetcher.getArticle(url)
+        gen = GenerateArticle()
+        gen.generate(article)sud
 
-		gen = GenerateArticle()
-		gen.generate(article)
+        interface = ArticleCommit()
+        interface.commit(article)
 
-		com = CommitArticle()
-		com.process(article)
+
+    def run(self):
+        url = "https://www.reddit.com/r/AskReddit/comments/3qynmv/you_gain_the_ability_to_put_a_30s_waiting_period/"
+        
+        self.createArticle(url)
 
 mycontroller = Controller()
 mycontroller.run()
