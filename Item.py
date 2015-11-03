f50acb78541ea6bdd4536731073e0a549ef8f133
Ashkan <ashkan.pk@gmail.com>
2015-11-03 15:04:14 -0500

    Cleaned up. Not using item obj anymore, a dict suffices

--- Item.py
+++
@@ -1,8 +0,0 @@
-class Item(object):
-	
-	def __init__(self, text, score=None, gif_url="", original_url=""):
-		self.score = score
-		self.text = text
-		self.gif_url = gif_url or ""
-		self.original_url = original_url or ""
-
