7251d07485bf7f600b74815ff9891d47e6f4d343
Ashkan <ashkan.pk@gmail.com>
2015-11-03 15:12:52 -0500

    cleaned FetchThread, builds article items as dicts now

--- FetchThread.py
+++ FetchThread.py
@@ -4,24 +4,23 @@ from Item import Item
 
 class FetchThread(object):
 
-	# Main method used to get thread details from Reddit
-	def getArticle(self, url):
-		sub = self.red_api.get_submission(url=url)
+    # Main method used to get thread details from Reddit
+    def getArticle(self, url):
+        sub = self.red_api.get_submission(url=url)
 
-		title = sub.title
-		url = url
-		score = sub.score
+        items = []
+        for idx in range(len(sub.comments)-1):
+            com = sub.comments[idx]
+            item = {
+                'text':  com.body,
+                'score': com.score
+            }
+            items.append(item)
+        
+        article = Article(sub.title, url, sub.score, items)
 
-		items = []
-		for idx in range(len(sub.comments)-1):
-			com = sub.comments[idx]
-			item = Item(com.body, com.score)
-			items.append(item)
-		
-		self.article = Article(title, url, score, items)
+        return article
 
-		return self.article
-
-	def __init__(self):
-		self.red_api = praw.Reddit('BuzzFeedBot')
-		pass
+    def __init__(self):
+        self.red_api = praw.Reddit('BuzzFeedBot')
+        pass
